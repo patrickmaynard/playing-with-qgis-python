4bc5ba9fc993b0d7faaa18e3c90091114a2d168b
Formerly borpglass <patrickmaynard@users.noreply.github.com>
2016-10-03 14:04:16 +0200

    Update test-two.py

--- test-two.py
+++ test-two.py
@@ -1,6 +1,10 @@
-#Here I am just testing the ability to load a layer.
+#Here I am just testing the ability to load a couple layers.
 #Modified from http://docs.qgis.org/testing/en/docs/pyqgis_developer_cookbook/loadlayer.html
 
-layer = iface.addVectorLayer("/Users/patrickmaynard/Downloads/mn_mappluto_16v1/MNMapPLUTO.shp", "PLUTO", "ogr")
-if not layer:
-  print "Layer failed to load!"
+layerPluto = iface.addVectorLayer("/Users/patrickmaynard/Downloads/mn_mappluto_16v1/MNMapPLUTO.shp", "PLUTO", "ogr")
+if not layerPluto:
+  print "layerPluto failed to load!"
+  
+layerEntrances = iface.addVectorLayer("/Users/patrickmaynard/Downloads/subway/geo_export_950003d9-f387-479c-9bf5-a1ca5a252bce.shp", "Entrances", "ogr")
+if not layerEntrances:
+  print "layerPluto failed to load!"
